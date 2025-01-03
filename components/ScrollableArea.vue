9b8111e989442b80939275adf17f43e82cc541aa
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-03 04:47:36 +0100

    feat(scrollableArea): create ScrollableArea.vue with direction prop for horizontal and vertical scrolling

---
+++ components/ScrollableArea.vue
@@ -0,0 +1,39 @@
+<template>
+  <section :class="scrollClass">
+    <slot></slot>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ScrollableArea",
+  props: {
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator: (value: string) => ["vertical", "horizontal"].includes(value),
+    },
+  },
+  computed: {
+    scrollClass() {
+      return this.direction === "horizontal"
+        ? "scroll-horizontal"
+        : "scroll-vertical";
+    },
+  },
+});
+</script>
+
+<style scoped>
+.scroll-vertical {
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.scroll-horizontal {
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+</style>
