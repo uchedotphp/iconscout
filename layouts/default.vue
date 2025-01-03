34651ae155df6cbb5458f2d1cefdfe88540af7fe
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-03 04:48:35 +0100

    feat(layout): create default.vue layout with TopHeader, TopSubHeader, and main content area

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <TopHeader />
+    <TopSubHeader class="sub-header" />
+    <main>
+      <div class="d-flex align-items-center secondary-navigation-area">
+        <!-- <FiltersMenu />
+        <ScrollableArea class="scroll-area">
+          <SecondaryNavigation />
+        </ScrollableArea> -->
+      </div>
+      <div>
+        <!-- <LeftFilterPanel /> -->
+        <nuxt :key="$route.fullPath" />
+      </div>
+    </main>
+    <!-- <BottomFooter /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DefaultLayout",
+});
+</script>
+
+<style lang="scss" scoped>
+.sub-header {
+  margin-top: 6px;
+}
+
+.secondary-navigation-area {
+  .scroll-area {
+    flex: 1;
+    padding-inline: 43px;
+  }
+}
+</style>
