71d339acd97d505b0322129969d771d2130a2e11
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-06 18:57:12 +0100

    feat(components): create FooterComponent with top and middle sections and scoped styles

---
+++ components/footer/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer class="footer">
+    <div class="mx-auto content">
+      <div class="top">
+        <div class="content">
+          <FooterTopArea max-width="1100px" />
+          <FooterMiddleArea max-width="1113px" class="middle" />
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="content">
+          <FooterBottomArea max-width="1100px" />
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FooterComponent",
+});
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  .top {
+    background-color: #ebedf5;
+  }
+  .middle {
+    padding-block: 24px;
+    border-top: 1px solid #D8DBEB;
+  }
+  .bottom {
+    background-color: #d8dbeb;
+    padding-block: 24px;
+  }
+}
+.content {
+  // max-width: 1100px;
+}
+</style>
