fe206fcbfc251d41ae2df565064bc4c22ccf7c07
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-08 18:25:44 +0100

    feat: Define Logo component using defineComponent
    - Import defineComponent from Vue
    - Define the Logo component using defineComponent

---
+++ components/base/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>
+    <NuxtLink to="/">
+      <img src="~/assets/images/iconScout-logo.svg" />
+    </NuxtLink>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Logo",
+});
+</script>
+
+<style scoped>
+/* .logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo img {
+  max-width: 100%;
+  height: auto;
+} */
+</style>
