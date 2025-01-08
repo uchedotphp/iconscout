889a313d689c73ad3da4dbd94ecf5ad8a1b3f3c6
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-08 18:38:54 +0100

    feat: Add navigation links to TopHeaderLinks component
    - Add template with a list of navigation links
    - Use v-for to iterate over navigationLinks and render each link
    - Include BaseBtn component for each link
    - Add conditional templates for iconRight and iconLeft based on link properties

--- components/topHeader/TopHeaderLinks.vue
+++ components/topHeader/TopHeaderLinks.vue
@@ -1,39 +1,29 @@
 <template>
   <ul class="d-flex align-items-center header-links">
-    <li>
-      <button class="d-flex align-items-center">
-        <span>
-          explore
-        </span>
-        <img src="~/assets/images/angle-down.svg" alt="angle-down arrow">
-      </button>
-    </li>
-    <li>
-      <button class="d-flex align-items-center">
-        <span>
-          tools
-        </span>
-        <img src="~/assets/images/angle-down.svg" alt="angle-down arrow">
-      </button>
-    </li>
-    <li>
-      <button>all features</button>
-    </li>
-    <li>
-      <button class="d-flex align-items-center">
-        <img src="~/assets/images/gift.svg" alt="angle-down arrow">
-        <span>
-          free asset
-        </span>
-      </button>
-    </li>
-    <li>
-      <button class="d-flex align-items-center">
-        <span>
-          learn
-        </span>
-        <img src="~/assets/images/angle-down.svg" alt="angle-down arrow">
-      </button>
+    <li v-for="{ title, id, isDropDown, img } in navigationLinks" :key="id">
+      <BaseBtn>
+        <template #label> {{ title }} </template>
+        <template v-if="isDropDown" #iconRight>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.2665 6.13359C10.9999 5.86693 10.5999 5.86693 10.3332 6.13359L7.99987 8.46693L5.66654 6.13359C5.39987 5.86693 4.99987 5.86693 4.7332 6.13359C4.46654 6.40026 4.46654 6.80026 4.7332 7.06693L7.5332 9.86693C7.66654 10.0003 7.79987 10.0669 7.99987 10.0669C8.19987 10.0669 8.3332 10.0003 8.46654 9.86693L11.2665 7.06693C11.5332 6.80026 11.5332 6.40026 11.2665 6.13359Z"
+              fill="#2E334C"
+            />
+          </svg>
+        </template>
+        <template v-if="title.toLocaleLowerCase() === 'free asset'" #iconLeft>
+          <img
+            :src="require(`@/assets/images/${img}`)"
+            :alt="formatAltText(img)"
+          />
+        </template>
+      </BaseBtn>
     </li>
   </ul>
 </template>
@@ -41,24 +31,63 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+interface NavigationLinks {
+  id: number;
+  title: string;
+  isDropDown: boolean;
+  img?: string;
+  link: string;
+}
+
 export default defineComponent({
   name: "TopHeaderLinks",
   data() {
     return {
-      // Add your data properties here if needed
+      navigationLinks: [] as NavigationLinks[],
     };
   },
-  methods: {
-    // Add your methods here if needed
+  created() {
+    this.navigationLinks = [
+      {
+        id: 1,
+        title: "explore",
+        isDropDown: true,
+        link: "#",
+      },
+      {
+        id: 2,
+        title: "tools",
+        isDropDown: true,
+        link: "#",
+      },
+      { id: 3, title: "all features", isDropDown: false, link: "#" },
+      {
+        id: 4,
+        title: "free asset",
+        isDropDown: false,
+        img: "gift.svg",
+        link: "#",
+      },
+      {
+        id: 5,
+        title: "learn",
+        isDropDown: true,
+        link: "#",
+      },
+    ];
   },
-  mounted() {
-    // Add your mounted logic here if needed
+  methods: {
+    formatAltText(img: string = ""): string {
+      // format this for the alt text displays
+      return img.replace(/-/g, " ").replace(/\.\w+$/, "");
+    },
   },
 });
 </script>
 
 <style lang="scss" scoped>
 ul.header-links {
+  list-style: none;
   li {
     margin-right: 16px;
     &:last-child {
@@ -71,6 +100,15 @@ ul.header-links {
       font-weight: 600;
       line-height: 19.78px;
       text-transform: capitalize;
+      padding: 0;
+      &:hover {
+        color: var(--ics-blue);
+        svg {
+          path {
+            fill: var(--ics-blue);
+          }
+        }
+      }
     }
   }
 }
