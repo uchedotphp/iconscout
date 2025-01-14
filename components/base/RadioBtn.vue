180a7e5d8ede12288dffed9843cc0a47a1a6ca4d
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-14 23:47:56 +0100

    update(RadioBtn): update radio button styles to fit design

--- components/base/RadioBtn.vue
+++ components/base/RadioBtn.vue
@@ -63,7 +63,7 @@ export default defineComponent({
 <style lang="scss" scoped>
 .radio-label {
   margin-bottom: 0;
-  padding-left: 8px;
+  padding-left: 12px;
   font-family: "Averta Std", sans-serif;
   font-size: 14px;
   font-weight: 400;
@@ -74,5 +74,35 @@ export default defineComponent({
     color: var(--ics-blue);
     cursor: pointer;
   }
+}
+
+input[type="radio"] {
+  appearance: none; /* Remove default styling */
+  -webkit-appearance: none; /* For Safari */
+  width: 16px;
+  height: 16px;
+  border: 1px solid #3D92DE;
+  border-radius: 50%;
+  outline: none;
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+}
+
+input[type="radio"]::before {
+  content: "";
+  width: 6px;
+  height: 6px;
+  background-color: #3D92DE;
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(0);
+  transition: transform 0.2s ease-in-out;
+}
+
+input[type="radio"]:checked::before {
+  transform: translate(-50%, -50%) scale(1);
 }
 </style>
